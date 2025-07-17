94cf48a139d25699f7dfe60819c1dbd28558918f
Rushi-Atrina <rushi@Rushis-MacBook-Air.local>
2025-07-18 01:16:52 +0530

    easy ocr package added

--- backend/functions/ocr.py
+++ backend/functions/ocr.py
@@ -1,6 +1,14 @@
-from PIL import Image
-import pytesseract
+# from PIL import Image
+# import pytesseract
+
+# def extract_text_from_image(image_path: str) -> str:
+#     return pytesseract.image_to_string(Image.open(image_path))
+
+import easyocr
+
+# Create the OCR reader only once
+reader = easyocr.Reader(['en'], gpu=False)
 
 def extract_text_from_image(image_path: str) -> str:
-    return pytesseract.image_to_string(Image.open(image_path))
-
+    results = reader.readtext(image_path)
+    return "\n".join([text for _, text, _ in results])
